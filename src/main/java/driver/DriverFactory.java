53fb03cf67c9203c852167b47f9db4f1654b1c3d
Basil Akavity <vakavity@gmail.com>
2024-07-01 10:09:43 +0300

    added driver

---
+++ src/main/java/driver/DriverFactory.java
@@ -0,0 +1,26 @@
+package driver;
+
+import enums.PlatformType;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class DriverFactory {
+    public AppiumDriver<MobileElement> createInstance(String platform) {
+
+        AppiumDriver<MobileElement> driver = null;
+
+        PlatformType mobilePlatform = PlatformType.valueOf(platform.toUpperCase());
+
+        switch (mobilePlatform) {
+
+            case ANDROID:
+                driver = new AndroidDriverManager().createInstance();
+                break;
+
+            case IOS:
+                driver = new IOSDriverManager().createInstance();
+                break;
+        }
+        return driver;
+    }
+}
