1d028f725e3611e94d764200e972c452c0e1f5d7
Basil Akavity <vakavity@gmail.com>
2024-07-16 18:42:30 +0300

    added swipeElementLeft, swipeElementLeft, swipeScreenLeft

--- src/main/java/org/akavity/utils/Utils.java
+++ src/main/java/org/akavity/utils/Utils.java
@@ -1,6 +1,12 @@
 package org.akavity.utils;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.appium.AppiumDriverRunner;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -29,5 +35,43 @@ public class Utils {
     public static void waitUntilClickable(SelenideElement element) {
         WebDriverWait wait = new WebDriverWait(getWebDriver(), Duration.ofMillis(30000));
         wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    public static void swipeElementLeft(int startX, int startY, int endX, int endY, int maxSwipeCounts) {
+        int count = 0;
+        do {
+            TouchAction touch = new TouchAction(AppiumDriverRunner.getAndroidDriver());
+            touch.press(PointOption.point(startX, startY))
+                    .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+                    .moveTo(PointOption.point(endX, endY))
+                    .release().perform();
+            count++;
+        } while (count < maxSwipeCounts);
+    }
+
+    public static void swipeElementLeft(int startX, int startY, int endX, int endY) {
+        int count = 0;
+        do {
+            TouchAction touch = new TouchAction(AppiumDriverRunner.getAndroidDriver());
+            touch.press(PointOption.point(startX, startY))
+                    .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+                    .moveTo(PointOption.point(endX, endY))
+                    .release().perform();
+            count++;
+        } while (count < 5);
+    }
+
+    public static void swipeScreenLeft() {
+        AndroidDriver driver = AppiumDriverRunner.getAndroidDriver();
+        Dimension dimension = driver.manage().window().getSize();
+        double startX = dimension.width * 0.8;
+        double startY = dimension.height * 0.8;
+        double endX = dimension.width * 0.2;
+        double endY = dimension.height * 0.8;
+        TouchAction touch = new TouchAction(driver);
+        touch.press(PointOption.point((int) startX, (int) startY))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(1)))
+                .moveTo(PointOption.point((int) endX, (int) endY))
+                .release().perform();
     }
 }
