4f5a406b262c4715df15101df6fe731c89a7b786
Basil Akavity <vakavity@gmail.com>
2024-08-14 14:14:19 +0300

    added settings

--- src/main/java/org/akavity/driver/AndroidDriver.java
+++ src/main/java/org/akavity/driver/AndroidDriver.java
@@ -11,6 +11,8 @@ import javax.annotation.ParametersAreNonnullByDefault;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @ParametersAreNonnullByDefault
 public class AndroidDriver implements WebDriverProvider {
@@ -19,9 +21,13 @@ public class AndroidDriver implements WebDriverProvider {
     @CheckReturnValue
     @Nonnull
     public WebDriver createDriver(Capabilities capabilities) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("ignoreUnimportantViews", false);
+        map.put("allowInvisibleElements", true);
 
         UiAutomator2Options options = new UiAutomator2Options();
         options.merge(capabilities);
+        options.setCapability("appium:settings", map);
         options.setPlatformName("Android");
         options.setPlatformVersion("14.0");
         options.setDeviceName("Medium Phone");
