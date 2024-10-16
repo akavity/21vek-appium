3036da31165f65cc8252c0bedd2f5c5962ebac99
Basil Akavity <vakavity@gmail.com>
2024-10-16 19:47:08 +0300

    add AppBarSteps

---
+++ src/main/java/org/akavity/steps/AppBarSteps.java
@@ -0,0 +1,32 @@
+package org.akavity.steps;
+
+import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
+import org.akavity.pages.AppBarPage;
+import org.akavity.utils.Utils;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.appium.AppiumClickOptions.tap;
+
+@Log4j2
+public class AppBarSteps {
+    AppBarPage appBarPage = new AppBarPage();
+
+    @Step
+    public void clickOnSearchField() {
+        log.info("Click on the search field");
+        appBarPage.getSearchField().shouldBe(visible);
+        appBarPage.getSearchField().click(tap());
+    }
+
+    @Step
+    public void enterTextIntoSearch(String text) {
+        log.info("Search text into the search");
+        appBarPage.getSearchField().click();
+        appBarPage.getSearchField().setValue(text);
+        appBarPage.getLoupeButton().click();
+
+
+        Utils.sleep(4000);
+    }
+}
