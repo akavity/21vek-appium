0e8b94b3ce223f7930fc13a05356479c26c974a9
Basil Akavity <vakavity@gmail.com>
2024-10-17 00:04:05 +0300

    added SearchData, searchData.json

---
+++ src/main/java/org/akavity/models/SearchData.java
@@ -0,0 +1,14 @@
+package org.akavity.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchData {
+    String text;
+    String item;
+    String title;
+}
